11397110c2d6a1dc749dfbad5e4950c5e33a5949
LuizGaming <45321324+Luizianho@users.noreply.github.com>
2024-01-01 03:20:31 +0100

    Test

--- Source/RoshaOnline/Private/RoshaGameInstance.cpp
+++ Source/RoshaOnline/Private/RoshaGameInstance.cpp
@@ -1,5 +1,61 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "RoshaGameInstance.h"
+#include "Http.h"
+
+void URoshaGameInstance::HttpPostRequest(FString URL, FString Content)
+{
+    // Utwórz obiekt ¿¹dania HTTP
+    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+
+    // Ustaw parametry ¿¹dania POST
+    HttpRequest->SetVerb("POST");
+    HttpRequest->SetURL(URL);
+    HttpRequest->SetHeader("Content-Type", "application/json"); // Ustaw odpowiedni nag³ówek w zale¿noœci od formatu danych
+    HttpRequest->SetContentAsString(Content);
+
+    // Ustaw callback dla zakoñczenia ¿¹dania
+    HttpRequest->OnProcessRequestComplete().BindUObject(this, &URoshaGameInstance::OnHttpRequestComplete);
+
+    // Wyœlij ¿¹danie
+    HttpRequest->ProcessRequest();
+}
+
+void URoshaGameInstance::HttpGetRequest(FString URL)
+{
+    // Utwórz obiekt ¿¹dania HTTP
+    TSharedRef<IHttpRequest, ESPMode::ThreadSafe> HttpRequest = FHttpModule::Get().CreateRequest();
+
+    // Ustaw parametry ¿¹dania
+    HttpRequest->SetVerb("GET");
+    HttpRequest->SetURL(URL);
+    HttpRequest->OnProcessRequestComplete().BindUObject(this, &URoshaGameInstance::OnHttpRequestComplete);
+
+    // Wyœlij ¿¹danie
+    HttpRequest->ProcessRequest();
+}
+
+void URoshaGameInstance::OnHttpRequestComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+    if (bWasSuccessful && Response.IsValid())
+    {
+        // Otrzymana treœæ odpowiedzi
+        FString Content = Response->GetContentAsString();
+        
+        UE_LOG(LogTemp, Log, TEXT("%s"), *Content);
+        //RequestStatus(Content);
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("Request failed!"));
+    }
+}
+
+void URoshaGameInstance::RequestStatus(FString status)
+{
+    if (status == "OK")
+    {
+        UE_LOG(LogTemp, Log, TEXT("OK!"));
+    }
+
+    
+}
 
--- Source/RoshaOnline/Public/RoshaGameInstance.h
+++ Source/RoshaOnline/Public/RoshaGameInstance.h
@@ -4,6 +4,11 @@
 
 #include "CoreMinimal.h"
 #include "Engine/GameInstance.h"
+#include "HttpModule.h"
+#include "Interfaces/IHttpRequest.h"
+#include "Interfaces/IHttpResponse.h"
+
+
 #include "RoshaGameInstance.generated.h"
 
 /**
@@ -13,5 +18,22 @@ UCLASS()
 class ROSHAONLINE_API URoshaGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
-	
+public:
+    // GET!!!!!!!!!!!
+    UFUNCTION(BlueprintCallable, Category = "HTTP")
+    void HttpGetRequest(FString URL);
+
+    // POST!!!!!!!!!!
+    UFUNCTION(BlueprintCallable, Category = "HTTP")
+    void HttpPostRequest(FString URL, FString Content);
+
+    // Callback dla zakoñczenia ¿¹dania HTTP
+    void OnHttpRequestComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
+
+    UFUNCTION(BlueprintCallable, Category = "HTTP")
+    void RequestStatus(FString status);
+
+    UPROPERTY()
+    FString getIP = "https://403.rosha.online/";
+
 };
