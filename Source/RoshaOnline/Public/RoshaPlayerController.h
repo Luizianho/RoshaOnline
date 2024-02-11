47895138af9e8c5938cceb17b07595fda096eb35
LuizGaming <45321324+Luizianho@users.noreply.github.com>
2024-02-11 22:37:49 +0100

    First Push

---
+++ Source/RoshaOnline/Public/RoshaPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "RoshaPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ROSHAONLINE_API ARoshaPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
