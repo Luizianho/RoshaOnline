9249008d28adee91dde986fdeb09669f1e51a138
LuizGaming <45321324+Luizianho@users.noreply.github.com>
2024-02-12 01:47:42 +0100

    Added IP address response from server

--- Source/RoshaOnline/Private/RoshaGameInstance.cpp
+++ Source/RoshaOnline/Private/RoshaGameInstance.cpp
@@ -2,4 +2,66 @@
 
 
 #include "RoshaGameInstance.h"
+#include "Http.h"
 
+void URoshaGameInstance::GetRoshaIPAddress()
+{
+	// Tworzê obiekt Request
+	TSharedRef<IHttpRequest> HttpRequest = FHttpModule::Get().CreateRequest();
+
+	// Je¿eli Request sie powiedzie, odpowiedŸ znajdziemy w funkcji HandleRoshaIPComplete();
+	HttpRequest->OnProcessRequestComplete().BindUObject(this, &URoshaGameInstance::HandleRoshaIPComplete);
+
+	// Link to pliczku PHP
+	FString URL = FString::Printf(TEXT("https://403.rosha.online/rosha_get_ip.php"));
+	HttpRequest->SetURL(URL);
+	HttpRequest->SetVerb("POST");
+	HttpRequest->SetHeader(TEXT("Content-Type"), TEXT("application/json"));
+
+	// Wysy³amy zapytanie
+	HttpRequest->ProcessRequest();
+}
+
+void URoshaGameInstance::HandleRoshaIPComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+    if (bWasSuccessful && Response.IsValid())
+    {
+        FString ResponseStr = Response->GetContentAsString();
+
+        TSharedPtr<FJsonObject> JsonObject = MakeShareable(new FJsonObject());
+        TSharedRef<TJsonReader<TCHAR>> JsonReader = TJsonReaderFactory<TCHAR>::Create(ResponseStr);
+
+        if (FJsonSerializer::Deserialize(JsonReader, JsonObject) && JsonObject.IsValid())
+        {
+            if (JsonObject->TryGetStringField("status", RoshaStatusPage))
+            {
+                // Je¿eli w PHP 'status' => 'OK'
+                if (RoshaStatusPage.Equals("OK", ESearchCase::IgnoreCase))
+                {
+                    // Je¿eli w PHP próbujemy pobraæ 'address' => '1.2.3.4'
+                    if (JsonObject->TryGetStringField("address", IPAddress))
+                    {
+                        // Poprawnie pobraliœmy IP
+                        UE_LOG(LogTemp, Warning, TEXT("Address: %s"), *IPAddress);
+                    }
+                    else
+                    {
+                        // Niepoprawny IP
+                        UE_LOG(LogTemp, Warning, TEXT("Invalid address: %s"), *IPAddress);
+                    }
+                }
+                else
+                {
+                    // B³¹d statusu pliku PHP 'status' != 'OK'
+                    UE_LOG(LogTemp, Warning, TEXT("Invalid Status: %s"), *RoshaStatusPage);
+                }
+            }
+        }
+
+        UE_LOG(LogTemp, Error, TEXT("Failed to parse response or status is not OK."));
+    }
+    else
+    {
+        UE_LOG(LogTemp, Error, TEXT("Failed to receive response from server."));
+    }
+}
--- Source/RoshaOnline/Public/RoshaGameInstance.h
+++ Source/RoshaOnline/Public/RoshaGameInstance.h
@@ -6,12 +6,27 @@
 #include "Engine/GameInstance.h"
 #include "RoshaGameInstance.generated.h"
 
-/**
- * 
- */
+class IHttpRequest;
+class IHttpResponse;
+
+typedef TSharedPtr<IHttpRequest, ESPMode::ThreadSafe> FHttpRequestPtr;
+typedef TSharedPtr<IHttpResponse, ESPMode::ThreadSafe> FHttpResponsePtr;
+
+
 UCLASS()
 class ROSHAONLINE_API URoshaGameInstance : public UGameInstance
 {
 	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Request")
+	void GetRoshaIPAddress();
+
+	void HandleRoshaIPComplete(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful);
 	
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FString IPAddress;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	FString RoshaStatusPage;
 };
